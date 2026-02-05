e3f51159a1b2798c15514c2444564f17748d7f9e
gyubeom2222 <gyubeom2222@gmail.com>
2026-02-05 13:30:36 +0900

    계산기 메인 함수 수정

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("안녕하세요")
+from add import add
+from subtract import subtract
+from multiply import multiply
+from divide import divide
+
+def calculator():
+    a = float(input("1st number: "))
+    b = float(input("2nd number: "))
+    op = input("연산자 (+, -, *, /): ")
+
+    operations = {
+        '+': add,
+        '-': subtract,
+        '*': multiply,
+        '/': divide
+    }
+
+    if op in operations:
+        try:
+            result = operations[op](a, b)
+            print("result:", result)
+        except Exception as e:
+            print("오류:", e)
+    else:
+        print("invalid operator.")
+
+if __name__ == "__main__":
+    calculator()
